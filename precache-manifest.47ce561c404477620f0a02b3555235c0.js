72217fc5e26d78a1631f4e45a14b0738987a4a2d
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-09-12 21:10:11 +0530

    Updates

---
+++ precache-manifest.47ce561c404477620f0a02b3555235c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3d894fead6f256b96a4e617ad30ad5",
+    "url": "/eco-project-front%20/index.html"
+  },
+  {
+    "revision": "5d87185039af67fa47dc",
+    "url": "/eco-project-front%20/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "682ce36710beac0a57c1",
+    "url": "/eco-project-front%20/static/css/main.099ce735.chunk.css"
+  },
+  {
+    "revision": "5d87185039af67fa47dc",
+    "url": "/eco-project-front%20/static/js/2.dca8de7f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/eco-project-front%20/static/js/2.dca8de7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682ce36710beac0a57c1",
+    "url": "/eco-project-front%20/static/js/main.f87da716.chunk.js"
+  },
+  {
+    "revision": "5f0a2e206e55806a9fad",
+    "url": "/eco-project-front%20/static/js/runtime-main.61eb15ef.js"
+  }
+]);
