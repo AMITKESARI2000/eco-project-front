a6579c34d1a28993b43d77bf0836f906868b1831
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-09-12 20:59:11 +0530

    Added Navigation Bar

---
+++ src/components/HomePage/PhotoCarousel/PhotoCarousel.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import './PhotoCarousel.css';
+
+const PhotoCarousel = () => {
+  const [state, setState] = useState({
+    indicators: true,
+    // fade: true,
+    touch: true,
+    pause: 'hover',
+    interval: 1000,
+    data: [],
+  });
+  return (
+    <div>
+      <Carousel {...state} className="container-PhotoCarousel">
+        <Carousel.Item>
+          <img
+            className="imageFeatured-PhotoCarousel"
+            src="https://images.unsplash.com/photo-1599394021759-afc65fd2eaaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1155&q=80"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="imageFeatured-PhotoCarousel"
+            src="https://images.unsplash.com/photo-1599394021759-afc65fd2eaaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1155&q=80"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="imageFeatured-PhotoCarousel"
+            src="https://images.unsplash.com/photo-1599394021759-afc65fd2eaaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1155&q=80"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default PhotoCarousel;
