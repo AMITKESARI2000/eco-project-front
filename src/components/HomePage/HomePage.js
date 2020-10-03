e77c98c69a5d07f8cc619d159451cd8341bf6273
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-10-04 00:13:26 +0530

    Preview Game added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import PhotoCarousel from './PhotoCarousel/PhotoCarousel';
 import PredictPol from './Home Predict Pollution/PredictPol';
 import ProjectFooter from './ProjectFooter/ProjectFooter';
 import SoilPollution from './Soil Pollution/SoilPollution';
+import PreviewGame from './Home Games/PreviewGame';
 
 const HomePage = () => {
   return (
@@ -12,6 +13,9 @@ const HomePage = () => {
       <PhotoCarousel />
       <SoilPollution />
       <PredictPol />
+      <PreviewGame />
+
+      {/* FOOTER */}
       <ProjectFooter />
     </div>
   );
