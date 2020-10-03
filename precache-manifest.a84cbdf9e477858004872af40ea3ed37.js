5c83f453c6c25ee0a3edc3b3f5def967c61b49bd
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-10-04 01:01:15 +0530

    Updates

---
+++ precache-manifest.a84cbdf9e477858004872af40ea3ed37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a58e61eaf8f9a5f296f50539ab561e",
+    "url": "/eco-project-front/index.html"
+  },
+  {
+    "revision": "81eb494057e60e8b149c",
+    "url": "/eco-project-front/static/css/2.3f1f1bc9.chunk.css"
+  },
+  {
+    "revision": "c256997fe815a6ca8e3f",
+    "url": "/eco-project-front/static/css/main.e0654e54.chunk.css"
+  },
+  {
+    "revision": "81eb494057e60e8b149c",
+    "url": "/eco-project-front/static/js/2.c8e18d38.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/eco-project-front/static/js/2.c8e18d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c256997fe815a6ca8e3f",
+    "url": "/eco-project-front/static/js/main.c8b9ff6c.chunk.js"
+  },
+  {
+    "revision": "2f2ed7e149040fedb86a",
+    "url": "/eco-project-front/static/js/runtime-main.035ceb54.js"
+  },
+  {
+    "revision": "95b648101464249edb305a6479980c49",
+    "url": "/eco-project-front/static/media/earthworm_image.95b64810.jpg"
+  }
+]);
