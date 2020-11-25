0063f9c0b75720d6ee05a4e95bb494994abf6369
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-11-25 20:42:26 +0530

    india map image

--- src/components/MemePage/MemePage.js
+++ src/components/MemePage/MemePage.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import NavigationBar from '../HomePage/NavigationBar/NavigationBar';
-import { Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import MemeCard from './MemeCard';
 import meme1 from './Meme images/meme1.jpg';
 import meme2 from './Meme images/meme2.jpg';
@@ -8,63 +8,65 @@ import meme3 from './Meme images/meme3.jpg';
 import meme4 from './Meme images/meme4.jpg';
 import meme5 from './Meme images/meme5.jpg';
 import meme6 from './Meme images/meme6.jpeg';
+import ProjectFooter from '../HomePage/ProjectFooter/ProjectFooter';
 
 const MemePage = () => {
-  const [cardBody, setCardBody] = useState ([
-    {
-      image: meme5,
-      heading: 'meme',
-      des: 'lorem20  ',
-      timeStamp: '20th Oct 2020',
-    },
-    {
-      image: meme2,
-      heading: 'meme',
-      des: 'lorem20  ',
-      timeStamp: '20th Oct 2020',
-    },
-    {
-      image: meme3,
-      heading: 'meme',
-      des: 'lorem20  ',
-      timeStamp: '20th Oct 2020',
-    },
-    {
-      image: meme4,
-      heading: 'meme',
-      des: 'lorem20  ',
-      timeStamp: '20th Oct 2020',
-    },
-    {
-      image: meme6,
-      heading: 'meme',
-      des: 'lorem20  ',
-      timeStamp: '20th Oct 2020',
-    },
-    {
-      image: meme1,
-      heading: 'meme',
-      des: 'lorem20  ',
-      timeStamp: '20th Oct 2020',
-    },
-  ]);
-  return (
-    <div style={{overflow: 'hidden'}}>
-      <NavigationBar />
-      <h2>
-        MEMEMEMEMEMEMEMEMEMEMEMMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEME
-      </h2>
-      <Row>
-        {cardBody.map (memeCard => {
-          return (
-            <Col md={4} key={Math.random ()}>
-              <MemeCard memeCard={memeCard} />
-            </Col>
-          );
-        })}
-      </Row>
-    </div>
-  );
+    const [cardBody, setCardBody] = useState([
+        {
+            image: meme5,
+            heading: 'meme',
+            des: 'lorem20  ',
+            timeStamp: '20th Oct 2020',
+        },
+        {
+            image: meme2,
+            heading: 'meme',
+            des: 'lorem20  ',
+            timeStamp: '20th Oct 2020',
+        },
+        {
+            image: meme3,
+            heading: 'meme',
+            des: 'lorem20  ',
+            timeStamp: '20th Oct 2020',
+        },
+        {
+            image: meme4,
+            heading: 'meme',
+            des: 'lorem20  ',
+            timeStamp: '20th Oct 2020',
+        },
+        {
+            image: meme6,
+            heading: 'meme',
+            des: 'lorem20  ',
+            timeStamp: '20th Oct 2020',
+        },
+        {
+            image: meme1,
+            heading: 'meme',
+            des: 'lorem20  ',
+            timeStamp: '20th Oct 2020',
+        },
+    ]);
+    return (
+        <div style={{ overflow: 'hidden' }}>
+            <NavigationBar />
+            <h2>
+                MEMEMEMEMEMEMEMEMEMEMEMMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEME
+            </h2>
+            <Row>
+                {cardBody.map((memeCard) => {
+                    return (
+                        <Col md={4} key={Math.random()}>
+                            <MemeCard memeCard={memeCard} />
+                        </Col>
+                    );
+                })}
+            </Row>
+            <ProjectFooter />
+        </div>
+    );
 };
 
 export default MemePage;
