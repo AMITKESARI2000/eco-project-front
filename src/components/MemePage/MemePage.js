19a3b30e13d83a577cb956795e1cc8164b32ee2f
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-11-28 19:56:58 +0530

    Merge conflict

--- src/components/MemePage/MemePage.js
+++ src/components/MemePage/MemePage.js
@@ -8,7 +8,6 @@ import meme3 from './Meme images/meme3.jpg';
 import meme4 from './Meme images/meme4.jpg';
 import meme5 from './Meme images/meme5.jpg';
 import meme6 from './Meme images/meme6.jpeg';
-<<<<<<< HEAD
 import meme7 from './Meme images/450px-Water_when_the_soil_is_porous.jpg';
 import meme8 from './Meme images/download.jpg';
 import meme9 from './Meme images/Hummus_or_Humus.jpg';
@@ -108,67 +107,6 @@ const MemePage = () => {
       </Row>
     </div>
   );
-=======
-import ProjectFooter from '../HomePage/ProjectFooter/ProjectFooter';
-
-const MemePage = () => {
-    const [cardBody, setCardBody] = useState([
-        {
-            image: meme5,
-            heading: 'meme',
-            des: 'lorem20  ',
-            timeStamp: '20th Oct 2020',
-        },
-        {
-            image: meme2,
-            heading: 'meme',
-            des: 'lorem20  ',
-            timeStamp: '20th Oct 2020',
-        },
-        {
-            image: meme3,
-            heading: 'meme',
-            des: 'lorem20  ',
-            timeStamp: '20th Oct 2020',
-        },
-        {
-            image: meme4,
-            heading: 'meme',
-            des: 'lorem20  ',
-            timeStamp: '20th Oct 2020',
-        },
-        {
-            image: meme6,
-            heading: 'meme',
-            des: 'lorem20  ',
-            timeStamp: '20th Oct 2020',
-        },
-        {
-            image: meme1,
-            heading: 'meme',
-            des: 'lorem20  ',
-            timeStamp: '20th Oct 2020',
-        },
-    ]);
-    return (
-        <div style={{ overflow: 'hidden' }}>
-            <NavigationBar />
-            <h2>
-                MEMEMEMEMEMEMEMEMEMEMEMMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEMEME
-            </h2>
-            <Row>
-                {cardBody.map((memeCard) => {
-                    return (
-                        <Col md={4} key={Math.random()}>
-                            <MemeCard memeCard={memeCard} />
-                        </Col>
-                    );
-                })}
-            </Row>
-            <ProjectFooter />
-        </div>
-    );
->>>>>>> 5d2d2f4fefa55c11a99e26dbf5340dd544729ec0
 };
 
 export default MemePage;
