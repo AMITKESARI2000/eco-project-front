3f36b89664224fb3b6fb7b5cf68de26657b685f5
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-11-29 21:17:15 +0530

    image styling in blogs

--- src/components/BlogsPage/comp/Blog.component.js
+++ src/components/BlogsPage/comp/Blog.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Row, Col } from 'react-bootstrap';
 
 const Blog = ({ blog, deleteExercise }) => {
     return (
@@ -8,31 +8,83 @@ const Blog = ({ blog, deleteExercise }) => {
             <Jumbotron
                 style={{
                     padding: '1%',
+                    position: 'relative',
+                    border: '0.5rem solid green',
                     background:
                         'linear-gradient(113deg,#f7ca5e,#f4c59b,#f5e9e9)',
                 }}
             >
-                <h3>{blog.heading}</h3>
-                <p>{blog.description}</p>
-                <img
-                    src={blog.imgLink}
-                    alt="Blog Img"
-                    style={{ position: 'relative', right: '0' }}
-                ></img>
-                <p>
-                    {blog.username} {blog.date.substring(0, 10)}{' '}
-                    <Link to={'/blogs/edit/' + blog._id}>edit</Link> |{' '}
-                    <a
-                        href="/"
-                        onClick={(e) => {
-                            deleteExercise(blog._id, e);
+                <Row
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        margin: '0',
+                    }}
+                >
+                    <Col
+                        md={4}
+                        xl={3}
+                        style={{
+                            padding: '0%',
+                            margin: '0%',
                         }}
                     >
-                        delete
-                    </a>
-                    <span> Citation: </span>
-                      <a href={blog.citation}>  {blog.citation}</a>
-                </p>
+                        {blog.imgLink && (
+                            <img
+                                src={blog.imgLink}
+                                alt="Blog Img"
+                                style={{
+                                    position: 'relative',
+                                    right: '0',
+                                    width: '22vw',
+                                }}
+                            ></img>
+                        )}
+                    </Col>
+                    <Col>
+                        {' '}
+                        <div>
+                            <h3>{blog.heading}</h3>
+                            <p>{blog.description}</p>
+
+                            <p>
+                                {blog.username} {blog.date.substring(0, 10)}{' '}
+                                {blog.citation && (
+                                    <span>
+                                        {' '}
+                                        Citation:
+                                        <a href={blog.citation}>
+                                            {' '}
+                                            {blog.citation}
+                                        </a>
+                                    </span>
+                                )}
+                                <div
+                                    style={{
+                                        display: 'inline-block',
+                                        position: 'absolute',
+                                        textAlign: 'right',
+                                        right: '1%',
+                                    }}
+                                >
+                                    <Link to={'/blogs/edit/' + blog._id}>
+                                        edit
+                                    </Link>{' '}
+                                    |{' '}
+                                    <a
+                                        href="/"
+                                        onClick={(e) => {
+                                            deleteExercise(blog._id, e);
+                                        }}
+                                    >
+                                        delete
+                                    </a>
+                                </div>
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
             </Jumbotron>
         </div>
     );
