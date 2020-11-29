ff79bb28aa4da133f7970b117ad73032db664909
pandaking <aditya9660sharma@gmail.com>
2020-11-29 23:22:02 +0530

    particle removed and work given

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -53,7 +53,7 @@ const AboutUS = () => {
       name: 'Ankit Ghildiyal ',
       task: 'Project write up and Content',
     },
-    {image: './404.png', name: ' L Sai Kritik', task: 'NONE'},
+    {image: './404.png', name: ' L Sai Kritik', task: 'pic'},
   ]);
   return (
     <div
@@ -62,101 +62,6 @@ const AboutUS = () => {
       }}
     >
       <NavigationBar />
-      <Particles
-        height="100em"
-        params={{
-          particles: {
-            number: {
-              value: 200,
-              density: {
-                enable: true,
-                value_area: 1803.4120608655228,
-              },
-            },
-            color: {
-              value: 'lime',
-            },
-            shape: {
-              type: 'circle',
-              stroke: {
-                width: 2,
-                color: '#000000',
-              },
-              polygon: {
-                nb_sides: 4,
-              },
-            },
-            opacity: {
-              value: 0.4008530152163807,
-              random: false,
-              anim: {
-                enable: false,
-                speed: 1,
-                opacity_min: 0.1,
-                sync: false,
-              },
-            },
-            size: {
-              value: 1.5,
-              random: true,
-              anim: {
-                enable: false,
-                speed: 40,
-                size_min: 0.1,
-                sync: false,
-              },
-            },
-            line_linked: {
-              enable: true,
-              distance: 0,
-              color: '#ffffff',
-              opacity: 0.3687847739990702,
-              width: 0.6413648243462091,
-            },
-          },
-          interactivity: {
-            detect_on: 'window',
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'repulse',
-              },
-              onclick: {
-                enable: false,
-                mode: 'bubble',
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 400,
-                line_linked: {
-                  opacity: 1,
-                },
-              },
-              bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
-                speed: 3,
-              },
-              repulse: {
-                distance: 100,
-                duration: 0.4,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
-              },
-            },
-          },
-          retina_detect: true,
-        }}
-        style={{zIndex: '-1', position: 'absolute'}}
-      />
 
       {/* <img src={"./amit.jpg"} /> */}
       <div>
